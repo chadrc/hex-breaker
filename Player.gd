073c4399e883e31166e77646443179666d19cd89
Chad Collins <chadrcollins91@gmail.com>
2020-06-06 19:18:52 -0400

    Initial commit

---
+++ Player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+
+export (int) var speed = 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+		
+		
+func _physics_process(delta):
+	var v = Vector2(0, 0)
+	if Input.is_action_pressed("ui_right"):
+		v.x += 1
+	
+	if Input.is_action_pressed("ui_left"):
+		v.x -= 1
+		
+	v = v.normalized() * speed
+		
+	v = move_and_slide(v)
