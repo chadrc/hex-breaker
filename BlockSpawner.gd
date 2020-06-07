b9f613f09f06d9135a1588598eabbc2c66487367
Chad Collins <chadrcollins91@gmail.com>
2020-06-07 14:45:34 -0400

    Now generating all rows of blocks

--- BlockSpawner.gd
+++ BlockSpawner.gd
@@ -15,41 +15,75 @@ func _ready():
 	initial_y = base_block.position.y
 	var collision = base_block.get_node("CollisionPolygon2D")
 	var win_width = ProjectSettings.get_setting("display/window/size/width")
+	var win_height= ProjectSettings.get_setting("display/window/size/height")
 	var left = 0
 	var right = win_width
+	var top = 0
+	var bottom = win_height
 	for point in collision.polygon:
 		left = max(point.x, left)
 		right = min(point.x, right)
+		top = max(point.y, top)
+		bottom = min(point.y, bottom)
 	
 	var block_width = abs(left - right) * base_block.scale.x
+	var block_height = abs(bottom - top) * base_block.scale.y
+	# height of rectangle inside hexagon
+	var block_inner_height = tan(deg2rad(30)) * (block_width / 2) * 2
+	# inner height plus top triangle of hexagon
+	var additional_height = block_inner_height + ((block_height - block_inner_height) / 2)
+	
 	var board_width = win_width - side_padding * 2
+	var board_height = base_block.position.y - top_padding
+	
 	var block_count = floor(board_width / block_width)
-	var first_x = (win_width / 2 - block_width * floor(block_count / 2))
+	var row_count = floor(board_height / additional_height)
+	
+	print(base_block.position.y)
+	print(block_height)
+	print(board_height)
+	
+	var first = Vector2(win_width / 2 - block_width * floor(block_count / 2), base_block.position.y)
 	if int(block_count) % 2 == 0:
 		# add half block width so center is between 2 blocks
 		# instead of in middle of a single block
-		first_x += block_width / 2
+		first.x += block_width / 2
 	
-	
-	for i in range(block_count):
-		# generate block board
-		var new_pos = Vector2(first_x + block_width * i, base_block.position.y)
-		var new = base_block.duplicate()
-		ball.connect("body_entered", new, "_on_Ball_body_entered")
-		new.position = new_pos
-		
-		add_child(new)
-		
 	var off_block_count = block_count - 1
-	var off_first = Vector2(first_x, base_block.position.y) + Vector2(1, 0).rotated(deg2rad(-60)) * block_width
+	var off_first = Vector2(first.x, base_block.position.y) + Vector2(1, 0).rotated(deg2rad(-60)) * block_width
 	
-	for i in range(off_block_count):
-		var new_pos = Vector2(off_first.x + block_width * i, off_first.y)
-		var new = base_block.duplicate()
-		ball.connect("body_entered", new, "_on_Ball_body_entered")
-		new.position = new_pos
+	print(row_count)
+	for r in range(row_count):
+		var group = floor(r / 2)
+		var row_first
+		var row_block_count
+		var y_offset = block_height * group + block_inner_height * group
+		if r % 2 == 0:
+			# regular row
+			row_first = Vector2(first.x, first.y - y_offset)
+			row_block_count = block_count
+		else:
+			# off row
+			row_first = Vector2(off_first.x, off_first.y - y_offset)
+			row_block_count = off_block_count
 		
-		add_child(new)
+		for i in range(row_block_count):
+			# generate block board
+			var new_pos = Vector2(row_first.x + block_width * i, row_first.y)
+			var new = base_block.duplicate()
+			ball.connect("body_entered", new, "_on_Ball_body_entered")
+			new.position = new_pos
+			
+			add_child(new)
+		
+	
+#	for i in range(off_block_count):
+#		var new_pos = Vector2(off_first.x + block_width * i, off_first.y)
+#		var new = base_block.duplicate()
+#		ball.connect("body_entered", new, "_on_Ball_body_entered")
+#		new.position = new_pos
+#
+#		add_child(new)
 		
 	# disable and hide base block 
 	base_block.hide()
