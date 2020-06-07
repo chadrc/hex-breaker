947606f3891ee97f1948a19620d9a17daf547cd4
Chad Collins <chadrcollins91@gmail.com>
2020-06-07 10:11:13 -0400

    Fixed position reset of ball

--- Ball.gd
+++ Ball.gd
@@ -6,18 +6,31 @@ export (int) var max_speed = 300
 export (int) var min_speed = 50
 
 var previous_dir = Vector2(0, 1)
+var original_position
+var reset = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	original_position = position
 	linear_velocity = previous_dir * initial_speed
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _integrate_forces(state):
+	if reset:
+		state.transform = Transform2D(0, original_position)
+		linear_velocity = previous_dir * initial_speed
+		reset = false
+		return
+	
 	if state.linear_velocity.length() > max_speed:
 		state.linear_velocity = state.linear_velocity.normalized() * max_speed
 	elif linear_velocity.length() < min_speed:
 		state.linear_velocity = previous_dir * min_speed
 		
 	previous_dir = state.linear_velocity.normalized()
+
+
+func _on_DeathBox_body_entered(body):
+	if body == self:
+		reset = true
