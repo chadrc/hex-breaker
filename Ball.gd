f1f56523f3edcc205779a720b73e9eb0826d8abb
Chad Collins <chadrcollins91@gmail.com>
2020-06-07 15:18:00 -0400

    Ball now waits for user input to start at begining and after death

--- Ball.gd
+++ Ball.gd
@@ -8,20 +8,31 @@ export (int) var min_speed = 50
 var previous_dir = Vector2(0, 1)
 var original_position
 var reset = false
+var waiting = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	original_position = position
-	linear_velocity = previous_dir * initial_speed
+	linear_velocity = Vector2.ZERO
+
+
+func _process(delta):
+	if waiting and Input.is_action_pressed("ui_accept"):
+		linear_velocity = previous_dir * initial_speed
+		waiting = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _integrate_forces(state):
+	if waiting:
+		return;
+		
 	if reset:
 		previous_dir = Vector2(0, 1)
 		state.transform = Transform2D(0, original_position)
-		linear_velocity = previous_dir * initial_speed
+		linear_velocity = Vector2.ZERO
 		reset = false
+		waiting = true
 		return
 	
 	if state.linear_velocity.length() > max_speed:
@@ -36,7 +47,3 @@ func _on_DeathBox_body_entered(body):
 	if body == self:
 		reset = true
 
-
-func _on_Ball_body_entered(body):
-	print()
-	pass # Replace with function body.
