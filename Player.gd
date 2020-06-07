7447c1b6d4a1d102c96a1663c60b5695ca2ca726
Chad Collins <chadrcollins91@gmail.com>
2020-06-06 20:56:45 -0400

    Limiting lower limit of player movement

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 export (int) var ceiling = 200
+export (int) var sub_amount = 50
 export (int) var speed = 500
 
 # Called when the node enters the scene tree for the first time.
@@ -19,9 +20,9 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_down"):
 		v.y += 1
 	
-	var height = ProjectSettings.get_setting("display/window/size/height")
-	var upper_limit = height - ceiling
-	var lower_limit = height + height / 2
+	var win_height = ProjectSettings.get_setting("display/window/size/height")
+	var upper_limit = win_height - ceiling
+	var lower_limit = win_height + sub_amount
 	if v.y < 0 and position.y < upper_limit:
 		v.y = 0
 	elif v.y > 0 and position.y > lower_limit:
