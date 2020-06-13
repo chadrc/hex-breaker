28012be39827c27f7b6f0ab62d3e5f0d12e47be9
Chad Collins <chadrcollins91@gmail.com>
2020-06-13 12:38:10 -0400

    Swapped out block asset for solid fill

--- Blocks/Block.gd
+++ Blocks/Block.gd
@@ -2,7 +2,7 @@ extends StaticBody2D
 
 signal destroyed
 
-onready var Hex = $"Hex"
+onready var fill = $'Node2D/Fill'
 onready var death_timer = $"Timer"
 
 var color = HexColor.Red
@@ -10,7 +10,7 @@ var dead = false
 
 func _ready():
 	add_to_group("blocks")
-	Hex.set_single_color(color)
+	fill.self_modulate = HexColor.color_for(color)
 	
 
 func get_color():
@@ -19,8 +19,8 @@ func get_color():
 
 func set_single_color(c):
 	color = c
-	if Hex:
-		Hex.set_single_color(c)
+	if fill:
+		fill.self_modulate = HexColor.color_for(color)
 
 
 func destroy():
