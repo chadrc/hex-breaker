b380331f4e6b859f14a58a95b106470f42c8ea1d
Chad Collins <chadrcollins91@gmail.com>
2020-06-20 11:16:20 -0400

    Updated extra ball with new color change logic

--- Ball/ExtraBall.gd
+++ Ball/ExtraBall.gd
@@ -6,7 +6,9 @@ export (int) var min_speed = 50
 export (float) var min_y_velocity = 10.0
 
 onready var BallSprite = $"Sprite"
+onready var change_color_timer = $'ChangeColorTimer'
 
+var contact_position = null
 var previous_dir = Vector2(0, 1)
 var color = null
 
@@ -22,6 +24,8 @@ func launch(dir):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _integrate_forces(state):
+	if state.get_contact_count() > 0:
+		contact_position = state.get_contact_local_position(0)
 	
 	var y = state.linear_velocity.y
 	if y > -min_y_velocity and y < 0:
@@ -57,6 +61,10 @@ func _on_ExtraBall_body_entered(body):
 	if body.is_in_group("balls"):
 		if color:
 			body.set_color(color)
-		
 		if body.name == "Ball": # primary ball
 			body.unwait()
+	elif body.is_in_group("player"):
+		# timer is not active
+		if change_color_timer.is_stopped():
+			set_color(body.get_color(contact_position))
+			change_color_timer.start()
