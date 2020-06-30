933674e3cbbbd2a3c48241b370d3515c33344aaa
Chad Collins <chadrcollins91@gmail.com>
2020-06-30 07:38:43 -0400

    Added visuals for the borders

--- Game/Stage.gd
+++ Game/Stage.gd
@@ -6,9 +6,9 @@ onready var right_border = $'Borders/Right'
 func _ready():
 	var width = size * (get_viewport().size.x / get_viewport().size.y)
 	var p = left_border.shape.extents.x / 2
-	var h = (size - width) / 2
-	var left = h + p
-	var right = size - h - p
+	var margin = (size - width) / 2
+	var left = margin + p
+	var right = size - margin - p
 	left_border.translation.x = left
 	right_border.translation.x = right
 
