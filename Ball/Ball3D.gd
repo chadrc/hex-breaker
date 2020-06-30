a16e04654d070e3ec583f890cf5a131cf525efbd
Chad Collins <chadrcollins91@gmail.com>
2020-06-30 07:49:30 -0400

    Removed ball energy and explosion mechanic

--- Ball/Ball3D.gd
+++ Ball/Ball3D.gd
@@ -2,19 +2,13 @@ extends RigidBody
 
 signal launched
 signal lost
-signal energy_update
 
 export (int) var initial_speed = 25
 export (int) var max_speed = 60
 export (int) var min_speed = 15
 export (float) var min_y_velocity = 10.0
-export (int) var max_energy = 100
-export (float) var energy_per_tick = .1
-export (int) var player_energy_loss = 10
-export (int) var block_energy_loss = 20
 
 onready var ball = $"Visuals/Disk"
-onready var energy_timer = $'EnergyTimer'
 onready var launch_timer = $'LaunchTimer'
 onready var change_color_timer = $'ChangeColorTimer'
 
@@ -24,9 +18,6 @@ var reset = false
 var waiting = true
 var color = HexColor.Red
 var reset_x
-var energy = 0
-var explode_targets = []
-var extra = false
 
 func set_color(c):
 	color = c
@@ -44,9 +35,7 @@ func unwait():
 	waiting = false
 	reset = false
 	contact_monitor = true
-	if !extra:
-		energy_timer.start()
-		emit_signal("launched")
+	emit_signal("launched")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -76,8 +65,6 @@ func _integrate_forces(state):
 		linear_velocity = Vector3.ZERO
 		reset = false
 		waiting = true
-		_set_energy(0)
-		energy_timer.stop()
 		launch_timer.start()
 		return
 	
@@ -99,19 +86,6 @@ func _integrate_forces(state):
 	previous_dir = state.linear_velocity.normalized()
 
 
-func _set_energy(amount):
-	energy = amount
-	emit_signal("energy_update", energy)
-
-
-func remove_energy(amount):
-	_set_energy(max(0, energy - amount))
-	
-
-func add_energy(amount):
-	_set_energy(min(energy + amount, max_energy))
-
-
 func _on_GameArea_reset(_colors):
 	reset = true
 	reset_x = original_translation.x
@@ -128,40 +102,17 @@ func _on_Ball_body_entered(body):
 	if body.is_in_group("blocks"):
 		if body.get_color() == color:
 			body.destroy()
-			remove_energy(block_energy_loss)
-		elif energy == max_energy:
-			for t in explode_targets:
-				t.destroy()
-			reset = true
-			reset_x = original_translation.x
 	elif body.is_in_group("player"):
 		# timer is not active
 		if change_color_timer.is_stopped():
 			set_color(body.get_color(translation))
-			remove_energy(player_energy_loss)
 			change_color_timer.start()
-	elif body.is_in_group("balls"):
-		# transfer color
-		pass
 		
 
 func _on_Player_recall_ability_invoked(player_pos):
 	if !waiting:
 		reset = true
 		reset_x = player_pos.x
-
-
-func _on_EnergyTimer_timeout():
-	add_energy(energy_per_tick)
-
-
-func _on_ExplodeArea_body_entered(body):
-	if body.is_in_group("blocks"):
-		explode_targets.append(body)
-
-
-func _on_ExplodeArea_body_exited(body):
-	explode_targets.erase(body)
 
 
 func _on_LaunchTimer_timeout():
--- Game/GameUI.gd
+++ Game/GameUI.gd
@@ -3,13 +3,11 @@ extends CanvasLayer
 
 onready var BoostCooldownText = $'CooldownPanel/BoostCooldownText'
 onready var RecallCooldownText = $'CooldownPanel/RecallCooldownText'
-onready var EnergyProgressBar = $'ProgressBar'
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	BoostCooldownText.text = "Boost: Ready"
 	RecallCooldownText.text = "Recall: Ready"
-	EnergyProgressBar.percent_visible = false
 
 
 func _on_Player_boost_cooldown_tick(time, total):
@@ -29,6 +27,3 @@ func _on_Player_recall_cooldown_tick(time, total):
 func _on_Player_recall_cooldown_end():
 	RecallCooldownText.text = "Recall: Ready"
 
-
-func _on_Ball_energy_update(amount):
-	EnergyProgressBar.value = amount
